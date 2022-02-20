56f41e80f47332b5e31905e6935ec57fa604ded4
ahpraveen <aha.praveen@gmail.com>
2022-02-19 20:06:45 -0500

    first commit

---
+++ src/test/java/com/example/webspringbootapp/WebspringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebspringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
