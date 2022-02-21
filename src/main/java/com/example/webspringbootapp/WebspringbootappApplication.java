1bea0879218a15fd398d9455fed6016f3ac602c0
ahpraveen <aha.praveen@gmail.com>
2022-02-21 13:01:24 -0500

    added author details

--- src/main/java/com/example/webspringbootapp/WebspringbootappApplication.java
+++ src/main/java/com/example/webspringbootapp/WebspringbootappApplication.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Author: Praveen Anna Haridas
+ */
 @SpringBootApplication
 @RestController
 public class WebspringbootappApplication {
