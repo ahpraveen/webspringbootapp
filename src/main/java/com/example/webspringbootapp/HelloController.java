18ade2b79f08d2e65eef3875dde86da17f4cde29
ahpraveen <aha.praveen@gmail.com>
2022-04-05 14:42:23 -0400

    Revert
    This reverts commit 354db4614d79ba7fc233470276f3e305d439b58b.

--- src/main/java/com/example/webspringbootapp/HelloController.java
+++ src/main/java/com/example/webspringbootapp/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String hello(@RequestParam(value="name", defaultValue = "Praveen Kumar") String name){
+    public String hello(@RequestParam(value="name", defaultValue = "Praveen") String name){
         return String.format("Hello %s", name);
     }
 }
